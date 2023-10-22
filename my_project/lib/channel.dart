28bed8b97108e2fcc54c647d29468d0db7a552f1
JianLiu08 <129233285+JianLiu08@users.noreply.github.com>
2023-10-22 18:52:39 -0400

    Cambios de host en la base de datos

--- my_project/lib/channel.dart
+++ my_project/lib/channel.dart
@@ -10,13 +10,14 @@ class MyProjectChannel extends ApplicationChannel {
     logger.onRecord.listen(
         (rec) => print("$rec ${rec.error ?? ""} ${rec.stackTrace ?? ""}"));
 
-      final dataModel = ManagedDataModel.fromCurrentMirrorSystem();
-      final persistentStore = PostgreSQLPersistentStore.fromConnectionInfo(
+    final dataModel = ManagedDataModel.fromCurrentMirrorSystem();
+    final persistentStore = PostgreSQLPersistentStore.fromConnectionInfo(
+
         ///Crea un objeto "context" que contiene la conexion a la base de datos y el modelo de datos
         //Datos para el ingreso al servidor de la base de datos
         "postgres",
         "Puchy5531",
-        "localhost",
+        "postgres-db",
         5432,
         "DirectoryAPP"); //definido en las "clases"
 
@@ -24,14 +25,11 @@ class MyProjectChannel extends ApplicationChannel {
   }
 
   //colocar aqui las rutas junto con su controlador que maneja la logica de la ruta
-  @override 
+  @override
   Controller get entryPoint => Router()
-
     ..route("/status").linkFunction((request) async {
       return Response.ok({"message": "pong"});
     })
-
     ..route("/directories").link(() => ClientController(context))
-
     ..route("/directories/:c_id").link(() => ClientController(context));
 }
